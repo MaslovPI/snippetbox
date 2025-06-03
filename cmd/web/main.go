55c6642745b25a98514affdc73095dbbfe7fb3f4
Pavel Maslov <dnfanl@gmail.com>
2025-06-03 17:26:40 +0200

    Add styles and JS to base.tmpl

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
@@ -14,6 +19,7 @@ func main() {
 
 	port := ":4000"
 	log.Println("Starting server on port ", port)
+
 	err := http.ListenAndServe(port, mux)
 	log.Fatal(err)
 }
